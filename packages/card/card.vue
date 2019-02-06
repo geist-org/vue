53bac28885cca66eeb593632d3a8b9e16a0c56b5
WittBulter <nanazuimeng123@gmail.com>
2019-02-06 18:23:54 +0800

    feat(card): add hoverable prop

--- packages/card/card.vue
+++ packages/card/card.vue
@@ -1,10 +1,22 @@
-<template>
-<div>card</div>
+<template lang="pug">
+  .zi-card(:class="[dark && 'zi-dark', !hoverable && $style.disableHover]")
+    slot
 </template>
 
 <script>
 export default {
   name: 'zi-card',
+
+  props: {
+    dark: { type: Boolean, default: false },
+    hoverable: { type: Boolean, default: true },
+  },
 }
 </script>
 
+<style module lang="stylus">
+.disableHover
+  box-shadow: initial !important
+  &:hover
+    box-shadow: initial !important
+</style>
