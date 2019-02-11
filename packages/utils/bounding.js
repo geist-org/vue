0dbb8e80e6fbdc7e71f80fd0dd18b21aa2159a56
WittBulter <nanazuimeng123@gmail.com>
2019-02-11 15:24:15 +0800

    feat(utils): add bounding func

---
+++ packages/utils/bounding.js
@@ -0,0 +1,14 @@
+export const getRealShape = el => {
+  const rect = el.getBoundingClientRect()
+  const { width, height } = window.getComputedStyle(el)
+  const getCSSStyleVal = (str, parentNum) => {
+    if (!str) return 0
+    const strVal = str.includes('px') ? +str.split('px')[0] :
+      str.includes('%') ? +str.split('%')[0] * parentNum * 0.01 : str
+    return Number.isNaN(+strVal) ? 0 : +strVal
+  }
+  return {
+    width: getCSSStyleVal(`${width}`, rect.width),
+    height: getCSSStyleVal(`${height}`, rect.height),
+  }
+}
--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,5 +1,7 @@
 import * as validator from './validator'
+import * as bounding from './bounding'
 
 export {
   validator,
+  bounding,
 }
