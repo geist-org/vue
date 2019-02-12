4832f892fe4bd6149bd8a8cdc1a4d3584c7ac7f8
WittBulter <nanazuimeng123@gmail.com>
2019-02-12 15:16:03 +0800

    style(collapse): remove unused

--- docs/examples/collapse/basic.vue
+++ docs/examples/collapse/basic.vue
@@ -1,12 +1,11 @@
 <template lang="pug">
-div
-  zi-collapse
-    zi-collapse-item(title="Question A")
-      p Lorem ipsum dolor sit amet,consectetur adipiscing elit,
-        |sed do eiusmod tempor incididunt ut labore
-        |et dolore magna aliqua. Ut enim ad minim veniam,
-        |quis nostrud exercitation ullamco laboris
-        |nisi ut aliquip ex ea commodo consequat.
+zi-collapse
+  zi-collapse-item(title="Question A")
+    p Lorem ipsum dolor sit amet,consectetur adipiscing elit,
+      |sed do eiusmod tempor incididunt ut labore
+      |et dolore magna aliqua. Ut enim ad minim veniam,
+      |quis nostrud exercitation ullamco laboris
+      |nisi ut aliquip ex ea commodo consequat.
 </template>
 
 <script>
