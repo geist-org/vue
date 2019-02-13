ff86abfc8b0ff8d5ab8a2f3a6a5d9cbfa5f2f95e
WittBulter <nanazuimeng123@gmail.com>
2019-02-13 15:58:13 +0800

    chore: migrate to vue 2.6

--- docs/examples/fieldset/custom.vue
+++ docs/examples/fieldset/custom.vue
@@ -2,7 +2,7 @@
 zi-fieldset
   h1 The Evil Rabbit Jumped over the Fence
   p The Evil Rabbit Jumped over the Fence
-  template(slot="footer")
+  template(v-slot:footer)
     p The Evil Rabbit Jumped over the Fence
     zi-button(size="mini" type="primary" auto) action
 </template>
--- docs/examples/fieldset/disabled.vue
+++ docs/examples/fieldset/disabled.vue
@@ -2,7 +2,7 @@
 zi-fieldset(disabled)
   h1 The Evil Rabbit Jumped over the Fence
   p The Evil Rabbit Jumped over the Fence
-  template(slot="footer")
+  template(v-slot:footer)
     p The Evil Rabbit Jumped over the Fence
     zi-button(size="mini" type="primary" auto disabled) action
 </template>
