72c7fc34b795cafceaf5a83ed3ad1c8bfadb3d52
sujiaheng <648952316@qq.com>
2019-02-14 13:48:12 +0800

    feat(tag): component and example

---
+++ docs/examples/tag/basic.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+div
+  zi-tag Status: Unstable
+  br
+  zi-tag(type="success") Status: Unstable
+  br
+  zi-tag(type="warning") Status: Unstable
+</template>
+<script>
+export default {
+  name: 'ex-tag-basic',
+}
+</script>
---
+++ packages/tag/tag.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+span.zi-tag(:class="type")
+  slot
+</template>
+<script>
+import { validator } from '../utils'
+
+export default {
+  name: 'zi-tag',
+
+  props: {
+    type: {
+      type: String,
+      validator: validator.enums(['success', 'warning', 'danger']),
+    },
+  },
+}
+</script>
