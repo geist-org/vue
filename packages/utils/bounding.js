12fbd587a613eed47aea65373d3e695b628ee332
WittBulter <nanazuimeng123@gmail.com>
2019-02-14 16:06:43 +0800

    fix(button_drip): fix drip position error

--- packages/utils/bounding.js
+++ packages/utils/bounding.js
@@ -5,10 +5,15 @@ export const getRealShape = el => {
     if (!str) return 0
     const strVal = str.includes('px') ? +str.split('px')[0] :
       str.includes('%') ? +str.split('%')[0] * parentNum * 0.01 : str
+
     return Number.isNaN(+strVal) ? 0 : +strVal
   }
   return {
     width: getCSSStyleVal(`${width}`, rect.width),
     height: getCSSStyleVal(`${height}`, rect.height),
   }
+}
+
+export const getRect = el => {
+  return el.getBoundingClientRect()
 }
