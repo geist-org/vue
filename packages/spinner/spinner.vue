2d9cf1ff9ada015f9b69686a57fdcad5d9f1d4a1
WittBulter <nanazuimeng123@gmail.com>
2019-02-15 11:40:22 +0800

    feat(spinner): add component and examples

---
+++ packages/spinner/spinner.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.zi-spinner(:class="[size && size, { auto }]")
+  .zi-spinner-container
+    .zi-spinner-bar(v-for="index in 12" :key="`zi-spinner-bar-${index}`")
+</template>
+
+<script>
+import { validator } from '../utils'
+
+export default {
+  name: 'zi-spinner',
+
+  props: {
+    size: {
+      type: String,
+      validator: validator.enums(['mini', 'small', 'normal', 'big']),
+    },
+    auto: Boolean,
+  },
+}
+</script>
