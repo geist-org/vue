5cb90c13a27818142f6b4ebeb643f9dc65eb3235
Lzumikonata <504048655@qq.com>
2019-03-07 14:44:31 +0800

    fix(progress): change the tips

--- docs/examples/progress/fixed.vue
+++ docs/examples/progress/fixed.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
 div
   zi-progress(:percentage="percentage" fix="top" color="#409eff")
-  zi-button(@click="percentage += 5") top
+  zi-button(@click="percentage += 5" size="small") loading progress
+  p this fixed progress bar is displayed at the top of the page.
 </template>
 
 <script>
