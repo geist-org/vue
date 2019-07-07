92f20522ad0b29576369662c525eed248d1ef7d3
WittBulter <lambdas.dev@gmail.com>
2019-07-08 01:39:36 +0800

    build: exclude entry file at compiling

--- scripts/build.js
+++ scripts/build.js
@@ -11,7 +11,11 @@ const cyan = (text) => `\u001b[36m${text}\u001b[39m`
 const getPackages = async () => {
   const files = await fs.readdir(packagePath)
   return files
-    .filter(name => name !== 'document-main.vue.js' && name !== 'utils')
+    .filter(name =>
+      name !== 'document-main.vue.js'
+      && name !== 'utils'
+      && name !== 'index.js'
+    )
 }
 
 const runPackage = async (name, count) => {
