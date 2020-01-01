bde214c08786c3a74a98bf5a7ee9f3a515d32b9e
WittBulter <lambdas.dev@gmail.com>
2020-01-01 17:54:23 +0800

    feat(description): add description component

---
+++ packages/description/description.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+dl.zi-description
+  dt.zi-description-title {{ title }}
+  dd.zi-description-content(v-if="hasSlot")
+    slot
+  dd.zi-description-content(v-else) {{ content }}
+</template>
+
+<script>
+export default {
+  name: 'zi-description',
+
+  props: {
+    title: String,
+    content: String,
+  },
+
+  computed: {
+    hasSlot() {
+      return !!this.$slots.default
+    },
+  },
+}
+</script>
+
+<style lang="stylus" src="./description.styl"/>
