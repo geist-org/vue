dfa51abcc3e07281029de5fe7779ed8207252ff7
unix <lambdas.dev@gmail.com>
2020-02-19 16:56:20 +0800

    feat(input): support for external icons

---
+++ docs/examples/input/icon.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+.icons
+  zi-input(placeholder="placeholder" size="mini")
+    template(v-slot:prefixIcon)
+      github
+
+  br
+  zi-input(placeholder="placeholder")
+    template(v-slot:prefixIcon)
+      github
+
+  br
+  zi-input(placeholder="placeholder" size="big")
+    template(v-slot:prefixIcon)
+      github
+  br
+  zi-input(placeholder="placeholder")
+    template(v-slot:suffixIcon)
+      gift
+</template>
+
+<script>
+import github from '@zeit-ui/vue-icons/packages/github'
+import gift from '@zeit-ui/vue-icons/packages/gift'
+
+export default {
+  name: 'ex-input-icon',
+
+  components: {
+    github, gift,
+  },
+}
+</script>
+
+<style scoped lang="stylus">
+.icons
+  >div
+    margin-bottom 14px
+</style>
