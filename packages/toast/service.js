4538bfe26278e23baaac274d1ce06cb3dfea01de
yuler <joyful4yu@gmail.com>
2020-03-12 15:53:20 +0800

    fix(toast): use transform instead of animation and add hover interactive

--- packages/toast/service.js
+++ packages/toast/service.js
@@ -15,6 +15,7 @@ const initArea = () => {
 const insertToArea = (area, el, id) => {
   el.setAttribute('id', `zi-toast-${id}`)
   area.appendChild(el)
+  setTimeout(() => el.classList.add('visible'), 0)
 }
 
 const destroyToast = (id, all) => {
