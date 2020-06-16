308eae8e5d9846ca62195bd3cc1887101151728b
wuke <504048655@qq.com>
2020-06-16 15:48:10 +0800

    fix(button): add style for tab selected & update style bug

--- packages/button/button.vue
+++ packages/button/button.vue
@@ -1,5 +1,10 @@
 <template lang="pug">
-button.zi-btn(@click="clickHandler" :class="classes")
+button.zi-btn(@click="clickHandler"
+  :class="classes"
+  :tabindex="tabindex"
+  @keyup.tab="isFocus = true"
+  @blur="isFocus = false"
+  )
   span.zi-loading-shim(v-if="loading")
     i
     i
@@ -45,6 +50,7 @@ export default {
 
   data: () => ({
     dripStyles: '',
+    isFocus: false,
   }),
 
   computed: {
@@ -58,6 +64,7 @@ export default {
       this.ghost && (str += ' ghost')
       this.type && (str += ` ${this.type}`)
       this.size && (str += ` ${this.size}`)
+      this.isFocus && (str += ' tab-selected')
       return str.trim()
     },
 
@@ -68,6 +75,11 @@ export default {
     showIconRight() {
       return this.iconRight && this.size !== 'mini' && !this.loading
     },
+
+    tabindex() {
+      if (this.disabled) return -1
+      return 0
+    },
   },
 
   components: { ZiButtonDrip },
@@ -75,6 +87,7 @@ export default {
   methods: {
     clickHandler(event) {
       if (this.loading || this.disabled) return
+      this.isFocus = false
       // hide drip when button in shadow status
       const showDrip = !this.shadow && !this.ghost && this.effect && this.$refs.drip
       showDrip && this.$refs.drip.trigger(event)
