9152db5d22577c9635338152204991ff15bc8deb
wuke <504048655@qq.com>
2020-06-22 18:10:50 +0800

    fix(dialog): correct the bug of dialog's body can't scroll
    fix: revert
    
    fix(dialog): update
    
    fix(dialog): update code style

--- docs/examples/snippet/basic.vue
+++ docs/examples/snippet/basic.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-zi-snippet(text="yarn add @zeit-ui/vue" width="300px" filled)
+zi-snippet(text="yarn add @zeit-ui/vue" width="300px")
 </template>
 
 <script>
--- packages/select/down-icon.vue
+++ packages/select/down-icon.vue
@@ -1,10 +1,11 @@
 <template>
   <svg viewBox="0 0 22 12" fill="none">
-    <path d="M1 1L11 11L21 1"
-          stroke="var(--geist-foreground)"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"></path>
+    <path
+      d="M1 1L11 11L21 1"
+      stroke="var(--geist-foreground)"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"></path>
   </svg>
 </template>
 
