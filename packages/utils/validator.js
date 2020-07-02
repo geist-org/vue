a5cfaedafe0a112633a7b5bcaa4440fccb2c459b
wuke <504048655@qq.com>
2020-07-02 21:38:50 +0800

    fix: fix scrollbar on windows & macOS
    fix: delete useless style
    
    fix: format code

--- packages/utils/validator.js
+++ packages/utils/validator.js
@@ -2,3 +2,9 @@ export const enums = params => val => {
   if (!Array.isArray(params)) return false
   return !!params.find(item => item === val)
 }
+
+export const isMac = () => {
+  if (typeof window === 'undefined' || !window.navigator) return false
+  const agent = navigator.userAgent.toLowerCase()
+  return /macintosh|mac os x/i.test(agent)
+}
