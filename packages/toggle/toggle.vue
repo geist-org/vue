73615538dd8df5d258b92cb69e85e16dbc9e1ec4
Yzy <954478466@qq.com>
2021-04-29 18:34:24 +0800

    Add toggle size property

--- packages/toggle/toggle.vue
+++ packages/toggle/toggle.vue
@@ -1,18 +1,43 @@
 <template lang="pug">
-.zi-toggle(@click="clickHandler" :class="{ checked: !!model, disabled }")
+.zi-toggle(@click="clickHandler" :class="[{ checked: !!model, disabled }, size]")
+  .inner(:style="{ width: innerSwitchRect, height: innerSwitchRect }")
 </template>
 
 <script>
 export default {
   name: 'zi-toggle',
+  beforeMount() {
+    const basic = '2px';
+    switch (this.size) {
+      case 'mini':
+        this.innerSwitchRect=  `calc(.835rem - ${basic})`;
+        break;
+      case 'medium':  
+        this.innerSwitchRect=  `calc(.875rem - ${basic})`;   
+        break;
+      case 'large':    
+        this.innerSwitchRect=  `calc(1rem - ${basic})`; 
+        break;           
+      default:
+        break;
+    }
+  },
 
   data: () => ({
     privateModel: undefined,
+    innerSwitchRect: '',
   }),
 
   props: {
     value: [String, Number, Boolean],
     disabled: Boolean,
+    size: {
+      type: String,
+      default: 'medium',
+      validator: function (value) {
+        return ['mini', 'medium', 'large'].indexOf(value) !== -1
+      }
+    }
   },
 
   computed: {
